29bb7020d00390151628cc078ed733c1198081c8
Arush-kshirsagar <71516847+Arush-kshirsagar@users.noreply.github.com>
2021-04-21 20:31:09 +0530

    Update box.js

--- box.js
+++ box.js
@@ -14,10 +14,16 @@ class Box{
       World.add(world, this.body);
     }
     display(){
-      var angle = this.body.angle;
-      push();
+      
       if(this.body.speed<3){
-        this.body.display();
+        var angle = this.body.angle;
+      push();
+         translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("orange")
+      rect( 0, 0, this.width, this.height);
+       pop();
       }
       else{
        World.remove(world,this.body);
@@ -25,12 +31,6 @@ class Box{
         this.visibility=this.visibility-5;
         tint(255,this.visibility);
       }
-      translate(this.body.position.x, this.body.position.y);
-      rotate(angle);
-      rectMode(CENTER);
-      fill("orange")
-      rect( 0, 0, this.width, this.height);
       
-      pop();
     }
   }
